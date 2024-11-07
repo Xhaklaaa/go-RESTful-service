f006322623aa2d23bb324df192bc3d528604e2b5
Vadim Suraev <Xhaklaaa@gmail.com>
2024-11-08 00:33:14 +0700

    Mux router update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-RESTful-service
 
 go 1.23.2
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // var bindAdrress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for server")
@@ -20,8 +22,18 @@ func main() {
 
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
+	putRouter.Use(ph.MiddlewareProductionValidation)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareProductionValidation)
 
 	s := &http.Server{
 		Addr:         ":9090",
